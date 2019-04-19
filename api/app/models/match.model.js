5f114497f96861d75e046902256aff9d2f7cb171
Vorleak Chy <vorleak.chy@gmail.com>
2019-04-19 18:44:41 -0500

    Add Match schema

---
+++ api/app/models/match.model.js
@@ -0,0 +1,92 @@
+const mongoose = require('mongoose');
+
+const MatchSchema = mongoose.Schema({
+    leagueName: String,
+    startDateTime: Date,
+    homeTeam: {
+        name: String,
+        coach: String,
+        players: [
+            {
+                name: String,
+                position: String,
+                number: Number
+            }
+        ],
+        substitutes: [
+            {
+                miniute: Number,
+                playerIn: String,
+                playerOut: String
+            }
+        ]
+    },
+    awayTeam: {
+        name: String,
+        coach: String,
+        players: [
+            {
+                name: String,
+                position: String,
+                number: Number
+            }
+        ],
+        substitutes: [
+            {
+                minute: Number,
+                playerIn: String,
+                playerOut: String
+            }
+        ]
+    },
+    status: String, // "", FT, HT
+    events: [
+        {
+            minute: Number,
+            player: String,
+            type: String, // Red, Yellow, Goal
+            homeScore: Number,
+            awayScore: Number
+        }
+    ],
+    commentaries: [
+        {
+            minute: Number,
+            message: String
+        }
+    ],
+    statistic: {
+        shotsOnTarget: {
+            homeTeam: Number,
+            awayTeam: Number
+        },
+        shotsOffTarget: {
+            homeTeam: Number,
+            awayTeam: Number
+        },
+        possession: {
+            homeTeam: Number,
+            awayTeam: Number
+        },
+        corners: {
+            homeTeam: Number,
+            awayTeam: Number
+        },
+        offsides: {
+            homeTeam: Number,
+            awayTeam: Number
+        },
+        fouls: {
+            homeTeam: Number,
+            awayTeam: Number
+        },
+        goalKicks: {
+            homeTeam: Number,
+            awayTeam: Number
+        }
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Match', UserSchema);
