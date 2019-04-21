067d2343715b735f08ca2ebe2c74a703efc304db
Vorleak Chy <vorleak.chy@gmail.com>
2019-04-21 16:19:06 -0500

    Add sign up validation

--- client/src/app/signup/signup.component.ts
+++ client/src/app/signup/signup.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { UserService } from '../user.service';
+import { MustMatch } from '../_helpers/must-match-validator';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'signup',
@@ -10,55 +12,59 @@ import { UserService } from '../user.service';
 })
 export class SignupComponent implements OnInit {
   myForm: FormGroup;
+  loading: boolean = false;
+  submitted: boolean = false;
 
-  constructor(private formBuilder: FormBuilder, private userService: UserService) {
-    this.myForm = formBuilder.group({
+  constructor(
+    private formBuilder: FormBuilder, 
+    private userService: UserService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.myForm = this.formBuilder.group({
       'firstName': ['', Validators.required],
       'lastName': ['', Validators.required],
-      'email': ['', [
-         Validators.required, 
-         Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
-        ]], 
-        // [this.validateEmailNotTaken.bind(this)]],
-      'password': ['', Validators.required],
-      'confirmPassword': ['', [Validators.required]],
-      // 'confirmPassword': ['', [Validators.required, this.validatePasswords]],
-      // 'acceptTerm': [true]
+      'email': ['', [Validators.required, Validators.email], 
+        [this.validateExistingEmail.bind(this)]],
+      'password': ['', [Validators.required, Validators.minLength(6)]],
+      'confirmPassword': ['', Validators.required]
+    }, {
+      validator: MustMatch('password', 'confirmPassword')
     });
   }
 
-  ngOnInit() {
-  }
+  get f() { return this.myForm.controls; }
 
   onSubmit() {
-    const user = this.myForm.value;
+    this.submitted = true;
 
-    if (this.myForm.valid) {
-      return this.userService.create(user)
-        .subscribe(data => console.log(data));
-    } else {
-      console.log('Invalid form');
+    if (this.myForm.invalid) {
+      return;
     }
+
+    this.loading = true;
+
+    return this.userService.create(this.myForm.value)
+      .subscribe(data => {
+        this.router.navigateByUrl('login');
+      }, error => {
+        this.loading = false;
+      });
   }
 
-  validateEmailNotTaken(control: FormControl): Promise<any> | Observable<any> {
+  validateExistingEmail(control: FormControl): Promise<any> | Observable<any> {
     const promise = new Promise<any>(
       (resolve, reject) => {
-        this.userService.checkEmailNotTaken(control.value)
+        this.userService.checkExistingEmail(control.value)
           .subscribe((res: any) => {
-            const result = res.emailNotTaken ? null : {emailTaken: true};
-            resolve(result);
+            resolve(null);
+          }, error => {
+            resolve({ emailExists: true });
           });
       }
     );
 
     return promise;
   }
-
-  // validatePasswords(group: FormGroup): { [s: string]: boolean } {
-  //   const pass = group.controls.password.value;
-  //   const confirmPass = group.controls.confirmPassword.value;
-
-  //   return pass === confirmPass ? null : { notSame: true };
-  // }
 }
