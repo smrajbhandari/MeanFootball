cb65fea084548ab0d805546c1ce5d3c6569599a3
edwingbd <edwingbd@gmail.com>
2019-04-22 19:37:16 -0500

    update with the changes for show the details but is not finished

--- client/src/app/match/match.module.ts
+++ client/src/app/match/match.module.ts
@@ -3,17 +3,19 @@ import { CommonModule } from '@angular/common';
 import { MatchComponent } from './match.component';
 import { ListMatchesComponent } from './list-matches/list-matches.component';
 import { MatchDetailsComponent } from './match-details/match-details.component';
-import { MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import {MaterialModule} from '../material.module';
+//import { MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
 
 @NgModule({
   declarations: [MatchComponent, 
     ListMatchesComponent, 
     MatchDetailsComponent],
   imports: [
-    CommonModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule
+     CommonModule,MaterialModule
+     //,
+    // MatTableModule,
+    // MatPaginatorModule,
+    // MatSortModule
   ],
   exports: [MatchComponent, ListMatchesComponent, MatchDetailsComponent],
   bootstrap: [],
